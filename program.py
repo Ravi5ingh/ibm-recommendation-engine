915d2c91562e5ec48a538ea71bd708f323aeda06
Ravi5ingh <ravisingh1203@hotmail.com>
2020-06-06 13:43:33 +0100

    Part II is ready for submission

--- program.py
+++ program.py
@@ -9,43 +9,59 @@ se.widen_df_display()
 articles = ut.read_csv('data/articles.csv')
 interactions = ut.read_csv('data/interactions.csv')
 
+# ##################################################################################
+# #   Part I: Exploratory Data Analysis
+# ##################################################################################
+#
+# # SECTION 1.1
+# max_views_by_user = se.get_max_views_by_user(interactions)
+# # se.show_num_article_interaction_distribution(interactions) UNCOMMENT ME BEFORE SUBMISSION!!
+#
+# # SECTION 1.2
+# print(f'median: {se.get_median_num_article_interaction(interactions)}')
+# print(f'max views by user: {se.get_max_num_article_interaction(interactions)}')
+#
+# # SECTION 1.3
+# articles = se.remove_dupes(articles)
+# articles.to_csv('data/articles.csv', index=False)
+#
+# # SECTION 1.4
+# unique_articles = se.get_num_articles_with_interaction(interactions)
+# total_articles = se.get_num_articles(articles)
+# unique_users = se.get_unique_users(interactions)
+# user_article_interactions = len(interactions)
+#
+# # SECTION 1.5
+# # max_views_by_user =
+# most_viewed_article_id = se.get_most_viewed_article_id(interactions)
+# most_viewed_article_id_frequency = se.get_most_viewed_article_views(interactions)
+#
+# # SECTION 1.6
+# print(f'50% of individuals have {se.get_median_num_article_interaction(interactions)} or fewer interactions.')
+# print(f'The total number of user-article interactions in the dataset is {user_article_interactions}.')
+# print(f'The maximum number of user-article interactions by any 1 user is {max_views_by_user}.')
+# print(f'The most viewed article in the dataset was viewed {most_viewed_article_id_frequency} times.')
+# print(f'The article_id of the most viewed article is {most_viewed_article_id}.')
+# print(f'The number of unique articles that have at least 1 rating {unique_articles}.')
+# print(f'The number of unique users in the dataset is {unique_users}')
+# print(f'The number of unique articles on the IBM platform: {total_articles}')
+
+
 ##################################################################################
-#   Part I: Exploratory Data Analysis
+#   Part II: Rank-Based Recommendations
 ##################################################################################
 
-# SECTION 1.1
-max_views_by_user = se.get_max_views_by_user(interactions)
-# se.show_num_article_interaction_distribution(interactions) UNCOMMENT ME BEFORE SUBMISSION!!
+# SECTION 2.1
+print(se.get_top_articles(10, interactions))
+print(se.get_top_article_ids(10, interactions))
 
-# SECTION 1.2
-print(f'median: {se.get_median_num_article_interaction(interactions)}')
-print(f'max views by user: {se.get_max_num_article_interaction(interactions)}')
-
-# SECTION 1.3
-articles = se.remove_dupes(articles)
-articles.to_csv('data/articles.csv', index=False)
-
-# SECTION 1.4
-unique_articles = se.get_num_articles_with_interaction(interactions)
-total_articles = se.get_num_articles(articles)
-unique_users = se.get_unique_users(interactions)
-user_article_interactions = len(interactions)
-
-# SECTION 1.5
-# max_views_by_user =
-most_viewed_article_id = se.get_most_viewed_article_id(interactions)
-most_viewed_article_id_frequency = se.get_most_viewed_article_views(interactions)
-
-# SECTION 1.6
-print(f'50% of individuals have {se.get_median_num_article_interaction(interactions)} or fewer interactions.')
-print(f'The total number of user-article interactions in the dataset is {user_article_interactions}.')
-print(f'The maximum number of user-article interactions by any 1 user is {max_views_by_user}.')
-print(f'The most viewed article in the dataset was viewed {most_viewed_article_id_frequency} times.')
-print(f'The article_id of the most viewed article is {most_viewed_article_id}.')
-print(f'The number of unique articles that have at least 1 rating {unique_articles}.')
-print(f'The number of unique users in the dataset is {unique_users}')
-print(f'The number of unique articles on the IBM platform: {total_articles}')
-
+# SECTION 2.2
+top_5 = se.get_top_articles(5, interactions)
+top_10 = se.get_top_articles(10, interactions)
+top_20 = se.get_top_articles(20, interactions)
+print(top_5)
+print(top_10)
+print(top_20)
 
 # titles = se.get_top_articles(10, interactions)
 #
